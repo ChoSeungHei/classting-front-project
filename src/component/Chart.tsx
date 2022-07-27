539137d12ac45270c32955fb1600fd013521d910
ChoSeungHei <kwa0403261@gmail.com>
2022-07-27 21:23:37 +0900

    Fix : chart dataset

--- src/component/Chart.tsx
+++ src/component/Chart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -19,37 +19,42 @@ ChartJS.register(
   Legend,
 );
 
-export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: 'top' as const,
-    },
-    title: {
-      display: true,
-      text: 'Chart.js Bar Chart',
-    },
-  },
-};
-
-const labels = ['Score'];
-const answer = Number(sessionStorage.getItem('answer'));
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: '정답',
-      data: [answer],
-      backgroundColor: 'rgba(53, 162, 235, 0.5)',
-    },
-    {
-      label: '오답',
-      data: [10 - answer],
-      backgroundColor: 'rgba(255, 99, 132, 0.5)',
-    },
-  ],
-};
-
 export const Chart = () => {
+  const [answer, setAnswer] = useState(0);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: '정오답 비율',
+      },
+    },
+  };
+
+  const labels = ['Score'];
+  useEffect(() => {
+    setAnswer(Number(sessionStorage.getItem('answer')));
+  }, []);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: '정답',
+        data: [answer],
+        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+      },
+      {
+        label: '오답',
+        data: [10 - answer],
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      },
+    ],
+  };
+
   return <Bar options={options} data={data} />;
 };
